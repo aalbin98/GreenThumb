43196030f94f3d55c932eb30cacbd9ded3827323
aalbin98 <albin.martensson@edu.newton.se>
2023-12-05 11:16:19 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace GreenThumb
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
