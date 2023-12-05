727e0bc1434da8d8ce6052937bc2e26f466b740d
aalbin98 <albin.martensson@edu.newton.se>
2023-12-05 23:06:34 +0100

    packages, models

---
+++ Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenThumb.Models
+{
+    internal class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
